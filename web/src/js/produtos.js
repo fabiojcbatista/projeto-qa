048e17fdfc314574037aea1383c983176b6826b0
fabiojcbatista <fabiojcbweb@gamil.com>
2025-03-04 21:36:58 -0300

    update and delete product

--- web/src/js/produtos.js
+++ web/src/js/produtos.js
@@ -43,6 +43,57 @@ function exibirProdutos(produtos) {
         `;
         tabelaProdutos.appendChild(row);
     });
+
+        // Adicionar evento de clique aos botões de edição
+        const botoesEditar = document.querySelectorAll('.btn-editar');
+        botoesEditar.forEach(botao => {
+            botao.addEventListener('click', async function() {
+                const produtoId = this.getAttribute('data-id');
+                const produto = await buscarProdutoPorId(produtoId);
+                preencherModal(produto);
+                abrirModalProdutos();
+            });
+        });
+
+ // Adicionar evento de clique aos botões de exclusão
+ const botoesExcluir = document.querySelectorAll('.btn-excluir');
+ botoesExcluir.forEach(botao => {
+     botao.addEventListener('click', async function() {
+         const produtoId = this.getAttribute('data-id');
+         await excluirProduto(produtoId);
+         await buscarProdutos();
+         mostrarSucesso('Produto excluído com sucesso');
+     });
+ });
+}
+
+// Função para buscar um produto por ID
+async function buscarProdutoPorId(produtoId) {
+    try {
+        const response = await fetch(`http://fabiojcb.atwebpages.com/projeto-qa/api/v1/products/${produtoId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao buscar produto');
+        }
+
+        return response.json();
+    } catch (error) {
+        mostrarErro('Erro ao tentar buscar o produto. Tente novamente mais tarde.');
+    }
+}
+
+// Função para preencher o modal com os dados do produto
+function preencherModal(produto) {
+    modal.codigo.value = produto.codProduto;
+    modal.nome.value = produto.nmProduto;
+    modal.quantidade.value = produto.qtProduto;
+    modal.valor.value = produto.vlProduto;
+    modal.dataCadastro.value = produto.dtProduto;
 }
 
 // Função para mostrar mensagens de erro
@@ -74,7 +125,11 @@ btnAdicionar.addEventListener('click', (e) =>{
 
 modal.btnSalvar.addEventListener('click', async (e) =>{
     e.preventDefault();
-    await cadastro();
+    if (modal.codigo.value) {
+        await atualizarProduto();
+    } else {
+        await cadastro();
+    }
     limparCampos();
     fecharModalProdutos();
     await buscarProdutos();
@@ -131,6 +186,61 @@ async function cadastro(){
 async function cadastroProduto(data) {
     const response = await fetch('http://fabiojcb.atwebpages.com/projeto-qa/api/v1/products', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    return response.json();
+}
+
+// Função para excluir um produto
+async function excluirProduto(produtoId) {
+    try {
+        const response = await fetch(`http://fabiojcb.atwebpages.com/projeto-qa/api/v1/products/${produtoId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao excluir produto');
+        }
+
+        return response.json();
+    } catch (error) {
+        mostrarErro('Erro ao tentar excluir o produto. Tente novamente mais tarde.');
+    }
+}
+
+// Função para atualizar um produto
+async function atualizarProduto() {
+    try {
+        const data = {
+            codProduto: modal.codigo.value,
+            nmProduto: modal.nome.value,
+            qtProduto: modal.quantidade.value,
+            vlProduto: modal.valor.value,
+            dtProduto: modal.dataCadastro.value
+        };
+        const response = await atualizarProdutoAPI(data);
+        if (!response) {
+            mostrarErro(response.message);     
+        } 
+    } catch (error) {
+        mostrarErro('Erro ao tentar atualizar o produto. Tente novamente mais tarde.');
+    }
+}
+
+async function atualizarProdutoAPI(data) {
+    const response = await fetch(`http://fabiojcb.atwebpages.com/projeto-qa/api/v1/products/${data.codProduto}`, {
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
