13d9a4d1ded65d1b2a19e84ea2b19bc6b1a98edc
fabiojcbatista <fabiojcbweb@gamil.com>
2025-06-28 21:25:56 -0300

    Incluindo Jogador

--- api/v1/dto/PlayerDTO.php
+++ api/v1/dto/PlayerDTO.php
@@ -1,21 +1,18 @@
 <?php
 
-class AccountDTO {
-    public $id;
-    public $descricao;
-    public $tipo;
+class PlayerDTO {
+    public $id_cartoleiro;
+    public $nome;
 
-    public function __construct($accountData) {
-        $this->id = $accountData['id'] ?? null;
-        $this->descricao = $accountData['descricao'] ?? null;
-        $this->tipo = $accountData['tipo'] ?? null;
+    public function __construct($playerData) {
+        $this->id_cartoleiro = $playerData['id_cartoleiro'] ?? null;
+        $this->nome = $playerData['nome'] ?? null;
     }
 
     public function toArray() {
         return [
-            'id' => $this->id,
-            'descricao' => $this->descricao,
-            'tipo' => $this->tipo
+            'id_cartoleiro' => $this->id_cartoleiro,
+            'nome' => $this->nome
         ];
     }
 }
