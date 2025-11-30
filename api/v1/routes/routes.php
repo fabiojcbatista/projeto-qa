8526957196bd621e13d471852be55f10c3f3280f
fabiojcbatista <fabiojcbweb@gamil.com>
2025-11-29 21:08:53 -0300

    docs: correção do arquivo documentado da api em swagger

--- api/v1/routes/routes.php
+++ api/v1/routes/routes.php
@@ -13,7 +13,7 @@ require_once __DIR__ . '/../controllers/TeamLeagueController.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 // Suporte para requisições OPTIONS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
